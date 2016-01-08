770ea9d8e98bfec0667cf40068e8bc85be91c3d7
John Hearn <jhearn@gmail.com>
2016-01-07 18:49:48 -0800

    vector (data & alg assignment) now on a playground branch

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,14 @@
-var XXXXXXX = require( './xxxxxxx' ).XXXXXXX;
+var Vector = require( './vector' ).Vector;
 
-console.log(  XXXXXXX("foo") );
+var v = new Vector();
 
-//alert("foo");
+console.log( "v initial ", v );
+
+console.log( v.push(3) );
+console.log( v.unshift(2) );
+console.log( v.unshift(1) );
+
+console.log("v bigger ", v );
+console.log( v.pop() );
+console.log( v.shift() );
+console.log( "v smaller ", v );
---
+++ www/js/vector.js
@@ -0,0 +1,71 @@
+var vector_module = {
+    Vector: function(length) {
+		if( typeof length === "undefined" ) {
+			length = 0;
+		} else if( isNaN( length ) ||
+				   ( length < 0 )  ||
+				   ( length % 1 === 0 ) ) {
+			console.error( "Vector size is not a nonnegative integer." );
+			return null;
+		}
+		
+		var my = {};
+				
+		my.length = length;
+		
+		my.push = function(value) {
+			my[ my.length.toString() ] = value;
+			my.length++;
+		    return true;
+	    };
+	    
+	    my.pop = function() {
+		    var value;
+		    if( my.length > 0 ){
+			    value = my[ my.length.toString() ];
+			    delete my[ my.length.toString() ];
+			    my.length--;
+		    } else {
+			    value = undefined;
+		    }
+		    return value;
+	    };
+	    
+	    my.shift = function() {
+		    var value;
+		    if( my.length > 0 ){
+			    value = my[ "0" ];
+			    for(var ii = 1; ii < my.length; ii++) {
+				    my[ ( ii - 1 ).toString() ] = my[ ii.toString() ];
+			    }
+			    delete my[ my.length.toString() ];
+		    } else {
+			    value = undefined;
+		    }
+		    return value;
+	    };
+	    
+	    my.unshift = function(value) {
+		    for( var ii = my.length; ii > 0 ; ii-- ) {
+				    my[ ii.toString() ] = my[ ( ii - 1 ).toString() ];
+			}
+			my["0"] = value;
+		    return true;
+	    };
+	    
+	    my.unique = function() {
+		    return null;
+	    };
+	    
+	return my;
+    }
+};
+
+module.exports = vector_module;
+
+
+
+
+
+
+
