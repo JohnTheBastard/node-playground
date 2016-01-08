9dab78077fb74ffcadb5069007d645ed438c34bb
John Hearn <jhearn@gmail.com>
2016-01-07 19:59:45 -0800

    more cleanup from merging vector into repo

--- www/js/fizzbuzz.js
+++ www/js/fizzbuzz.js
@@ -1,20 +1,22 @@
-function fizzbuzz(n) {
-	var f = 'fizz';
-	var b = 'buzz';
-	
-	function divisibleBy(divisor) { 
-		return function(dividend) { 
-			return ( (dividend % divisor) === 0 );
-		}; 
-	}
-	
-	return [...Array(n)].map((x, ii) => {
-		ii++;
-		var value = '';
-		if ( divisibleBy(3)(ii) ) value = f;
-		if ( divisibleBy(5)(ii) ) value += b;
-		return value || n;
-	});	
-}
+var fizzbuzz_module = {
+	fizzbuzz: function (n) {
+		var f = 'fizz';
+		var b = 'buzz';
 
-console.log(fizzbuzz(25));
+		function divisibleBy(divisor) { 
+			return function(dividend) { 
+				return ( (dividend % divisor) === 0 );
+			}; 
+		}
+	
+		return [...Array(n)].map((x, ii) => {
+			ii++;
+			var value = '';
+			if ( divisibleBy(3)(ii) ) { value = f; }
+			if ( divisibleBy(5)(ii) ) { value += b; }
+			return value || ii;
+		});	
+	}
+};
+
+module.exports = fizzbuzz_module;
