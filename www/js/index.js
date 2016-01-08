32ba3cb172c5ebb15ce5fd5bbd3abd09f15c9422
John Hearn <jhearn@gmail.com>
2016-01-08 12:35:52 -0800

    initial commit for secretsanta (currently crashes)

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,24 @@
-var XXXXXXX = require( './xxxxxxx' ).XXXXXXX;
+var secretSanta = require( './secretSanta' ).secretSanta;
 
-console.log(  XXXXXXX("foo") );
+var recipients =  [ [ "Sean" ],
+					[ "Winnie" ],
+					[ "Brian", "Amy" ],
+					[ "Samir" ],
+					[ "Joe", "Bethany" ],
+					[ "Bruno", "Anna", "Matthew", "Lucas" ],
+					[ "Gabriel", "Martha", "Philip" ],
+					[ "Andre" ],
+					[ "Danielle" ],
+					[ "Leo", "Cinthia" ],
+					[ "Paula" ],
+					[ "Mary", "Jane" ],
+					[ "Anderson" ],
+					[ "Priscilla" ],
+					[ "Regis", "Julianna", "Arthur" ],
+					[ "Mark", "Marina" ],
+					[ "Alex", "Andrea" ] ];
+
+
+console.log(  secretSanta(recipients) );
 
 //alert("foo");
---
+++ www/js/secretSanta.js
@@ -0,0 +1,60 @@
+var secretSanta_module = {
+	secretSanta: function (recipients) {
+		var descending = function( left, right) { return left.length - right.length; };
+		// Sort recipients by family size, smallest to largest
+		recipients = recipients.sort(descending);
+		
+		
+		var count = function(recipients) {
+			return recipients.map( (a) => a.length).reduce( (pv, cv) => pv+cv );
+		};
+		
+		
+		var currentFamily = [];
+		var exchangeFamily = [];
+		var exchanges = {};
+		var leftovers = [];
+		
+		while ( count(recipient) > 3 ) {
+			// Get the largest unmatched family
+			currentFamily = recipients.pop();
+			
+			// Get members of the next biggest families to exchange with
+			while( exchangeFamily.length < currentFamily.length ) {
+				
+				// If we have leftovers from a previous iteration, use them
+				while( leftover.length > 0 ) {
+					exchangeFamily.push(leftovers.pop());
+				}
+				
+				exchangeFamily = exchangeFamily.concat( recipients.pop() );
+			} 
+			
+			// if we pulled in too many family members, save the leftovers for the next iteration
+			while ( exchangeFamily.length > currentFamily.length) {
+				leftovers.push( exchangeFamily.pop() );
+			}
+			
+			// assert( exchangeFamily.length = currentFamily.length );
+			
+			for ( var ii = 0; ii < currentFamily.length; ii++ ) {
+				exchanges[ currentFamily[ii] ] = exchangeFamily[ii];
+				exchanges[ exchangeFamily[ii] ] = currentFamily[ (ii+1) % currentFamily.length ];
+			}
+			
+			// we're done with these, so get ready for next iteration
+			currentFamily = [];
+			exchangeFamily = [];
+		}
+		
+		// assert( count(recipient) < 4 )
+		
+		
+		
+		return count(recipients);
+		
+		
+	}
+};
+
+module.exports = secretSanta_module;
