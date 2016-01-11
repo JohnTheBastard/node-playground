3bf02712a3edc5662f38f82e02ec5e0b9367c1a7
John Hearn <jhearn@gmail.com>
2016-01-11 15:37:47 -0800

    removed some defuct bitmapTransform code from when that module was pulled in from it's own repo so branch can have a clean pull request

--- www/js/bitmapTransformer.js
+++ www/js/bitmapTransformer.js
@@ -2,40 +2,6 @@ var FS = require('fs');
 
 var bitmapTransformer_module = {
     BitmapTransformer: function( pathToFile ) {
-		var self = this;
-	    //self.buffer = new Buffer( 0x2B46 );     // 0x2B46 is the size of our file
-
-		self.getBufferData = function( file, async_option ) {
-			if( async_option == 'async' ) {
-				FS.readFile( file, ( error, data ) => {
-					if (error) throw error;
-				return data;
-				});
-			} else {
-				return FS.readFileSync( file );     
-			}
-	    };
-	    
-	    self.buffer = self.getBufferData( pathToFile );
-	    
-	    
-
-console.log( self.buffer instanceof Buffer);
-
-console.log("Int8: ",  self.buffer.readInt8( 0x0 ).toString(16) );
-console.log("LE16: ",  self.buffer.readUInt16LE( 0x0 ).toString(16) );
-console.log("BE16: ",  self.buffer.readUInt16BE( 0x0 ).toString(16) );
-
-
-
-
-
-
-
-
-
-
-
 
     }
 };
