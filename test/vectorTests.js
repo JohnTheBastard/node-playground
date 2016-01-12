bbdcb6234474c03ee8b6b87b30cbe3add8249b2f
John Hearn <jhearn@gmail.com>
2016-01-11 18:22:55 -0800

    working on unique method tests

--- test/vectorTests.js
+++ test/vectorTests.js
@@ -6,16 +6,41 @@ var v = new Vector(0);
 var a = [];
 
 var dupes = [ "John", "John", "Jeff", "Geoff", "John", "George", "Joshua", "Jonathan", "John", "Josh", "Jeff" ];
+var noDupes = [ 'Geoff', 'George', 'Jeff', 'John', 'Jonathan', 'Josh', 'Joshua' ];
+
+function compare(v, a) {
+	assert( v.length === a.length );
+	for (var ii = 0; ii < a.length; ii++) {
+		assert( v[0] === a[0] );
+	}
+}
 
 
 describe( 'Vector object methods ', function() {
     it( 'should behave like Array.prototype methods', function(){
-	    v.push("foo");
-	    a.push("foo");
-		assert( v.length === a.length );
-		assert( v[0] === a[0] );
-    });
-    it( 'should also ...', function(){
-		assert( true );
+		v.push("foo");
+		a.push("foo");
+		v.push("bar");
+		a.push("bar");
+		v.unshift("bat");
+		a.unshift("bat");
+		v.unshift("baz");
+		a.unshift("baz");	  
+		compare(v, a);
+		
+		assert(v.pop() === a.pop());
+		compare(v, a);
+		assert(v.shift() === a.shift());
+		compare(v, a);
+		assert(v.pop() === a.pop());
+		compare(v, a);
+		assert(v.shift() === a.shift());
+		compare(v, a);
+		
+	});
+    it( 'Unique method', function(){
+	    console.log( dupes.unique() );
+	    console.log( noDupes );
+		assert( dupes.unique().equals(noDupes) );
     });
 });
--- www/js/index.js
+++ www/js/index.js
@@ -3,29 +3,26 @@ Array.prototype.unique = require( './vector' ).unique;
 
 var v = new Vector();
 
-console.log( "v initial ", v );
 
 v.push(3);
-console.log( "push 3: ", v );
 v.push(2);
-console.log( "push 2: ", v );
 v.unshift(1);
-console.log( "unshift 1: ", v );
 v.unshift(0);
-console.log( "unshift 0: ", v );
 
 var popped = v.pop();
-console.log("popped: ", popped );
-console.log( "pop 2: ", v );
+//console.log("popped: ", popped );
+//console.log( "pop 2: ", v );
 
 var shifted = v.shift();
-console.log( "shifted: ", shifted );
-console.log( "shift 0: ", v );
+//console.log( "shifted: ", shifted );
+//console.log( "shift 0: ", v );
 
 
 var dupes = [ "John", "John", "Jeff", "Geoff", 
 			  "John", "George", "Joshua", 
 			  "Jonathan", "John", "Josh", "Jeff" ];
+			  
+//console.log(dupes.sort());
 
 var noDupes = dupes.unique();
 //console.log(noDupes);
