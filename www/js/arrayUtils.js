5f2d83052c1b4f7945564363649399ff118e400c
John Hearn <jhearn@gmail.com>
2016-01-13 16:31:07 -0800

    adding arrayUtils to repo (again?)

---
+++ www/js/arrayUtils.js
@@ -0,0 +1,30 @@
+var array_utils_module = {
+	unique: function() {
+		var self = this;
+		var noDupes = [];
+		self.sort().map( function( value, index, collection ){
+			if (value !== collection[index - 1]) { noDupes.push(value); }
+			return value;
+		});
+		return noDupes;
+	},
+	
+	shuffle: function () {
+		return this.map( n => [ Math.random(), n ] )
+				   .sort()
+				   .map( n => n[1] );
+	},
+	
+	equals: function (array) {
+		// wait, I think I should be using !== not != ...
+	    if (!array || (this.length != array.length) ) { return false; }
+		for (var i = 0, l=this.length; i < l; i++) {
+			if (this[i] instanceof Array && array[i] instanceof Array) {
+				if ( !this[i].equals(array[i]) ) { return false; }      
+	        } else if ( this[i] != array[i] ) { return false; }           
+		}       
+		return true;
+	}	
+};
+
+module.exports = array_utils_module;
