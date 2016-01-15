bffacd40a67ec3c53de33077e11375fdfa1de3d5
John Hearn <jhearn@gmail.com>
2016-01-15 10:06:23 -0800

    removed arrayUtils bc .unique() isn't actually needed

--- www/my_modules/arrayUtils.js
+++ www/my_modules/arrayUtils.js
@@ -1,3 +1,18 @@
+/*
+const utils = require( './arrayUtils' );
+// Warn if overriding
+if(Array.prototype.equals)  { console.warn( "Overriding existing Array.prototype.equals." );  }
+if(Array.prototype.shuffle) { console.warn( "Overriding existing Array.prototype.shuffle." ); }
+if(Array.prototype.unique)  { console.warn( "Overriding existing Array.prototype.unique." );  }
+Array.prototype.equals = utils.equals;
+Array.prototype.shuffle = utils.shuffle;
+Array.prototype.unique = utils.unique;
+// Hide from for-in loops
+Object.defineProperty( Array.prototype, "equals",  { enumerable: false } );
+Object.defineProperty( Array.prototype, "shuffle", { enumerable: false } );
+Object.defineProperty( Array.prototype, "unique",  { enumerable: false } );
+*/
+
 var array_utils_module = {
 	unique: function() {
 		var self = this;
--- www/my_modules/snond.js
+++ www/my_modules/snond.js
@@ -1,27 +1,13 @@
-const utils = require( './arrayUtils' );
-// Warn if overriding
-if(Array.prototype.equals)  { console.warn( "Overriding existing Array.prototype.equals." );  }
-if(Array.prototype.shuffle) { console.warn( "Overriding existing Array.prototype.shuffle." ); }
-if(Array.prototype.unique)  { console.warn( "Overriding existing Array.prototype.unique." );  }
-Array.prototype.equals = utils.equals;
-Array.prototype.shuffle = utils.shuffle;
-Array.prototype.unique = utils.unique;
-// Hide from for-in loops
-Object.defineProperty( Array.prototype, "equals",  { enumerable: false } );
-Object.defineProperty( Array.prototype, "shuffle", { enumerable: false } );
-Object.defineProperty( Array.prototype, "unique",  { enumerable: false } );
-
 var fs = require('fs');
 var enable1 = require('path').join( __dirname, 'text/enable1.txt' );
 var dictionary = fs.readFileSync( enable1, 'utf-8' ).toString().split('\r\n');
 
 var snond = function ( secret, bad ) {
 	var secretArray = secret.split("");
-	var problemLetters = bad.split("").unique();
+	var problemLetters = bad.split("");
 	var subSecret = secretArray.filter( (c) => ( problemLetters.indexOf(c) > -1 ) ).join("");
 	return ( subSecret === bad );
 };
-
 var probCount = function (bad) { return dictionary.filter( (w) => snond(w, bad) ).length; };
 
 var snond_module = {
