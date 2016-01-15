6bf600b46d1470e0bfe4dad55ab50fe4729d8f0d
John Hearn <jhearn@gmail.com>
2016-01-15 09:56:27 -0800

    minor cleanup

--- www/my_modules/index.js
+++ www/my_modules/index.js
@@ -1,13 +1,14 @@
 var snond = require( './snond' ).snond;
 var problemCount = require( './snond' ).problemCount;
 
-var secretWords = [ "synchronized", "misfunctioned", "mispronounced", "shotgunned", "snond" ] ;
+var secretWords = [ "synchronized", "misfunctioned", 
+					"mispronounced", "shotgunned", 
+					"snond" ] ;
 var badWord = "snond";
 
-
 secretWords.map( (w) => console.log(  snond( w, badWord ) ) );
-console.log(  "snond: ", problemCount( "snond" ) );
-console.log(  "rrizi: ", problemCount( "rrizi" ) );
+console.log(  "\n  snond problem count: ", problemCount( "snond" ) );
+console.log(  "  rrizi problem count: ", problemCount( "rrizi" ) );
 
 
 
