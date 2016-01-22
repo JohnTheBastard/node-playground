568e494f3c40d93d821e912294e88c17e5629ff9
John Hearn <jhearn@gmail.com>
2016-01-21 17:57:09 -0800

    magicFruit iterator is now also a generator

--- client/my_modules/magicFruit.js
+++ client/my_modules/magicFruit.js
@@ -1,24 +1,28 @@
 "use strict";
 const magicFruit_module = {
-	magicFruit(population, fruit) {
+	magicFruit: function(population, fruit) {
 		let cur = new Array(fruit).fill(0),
 			ii = 1,
 			sum = (pv,cv)=>pv+cv,
-			inc = (cv) => ++cv,
-			fruitIter = { [ Symbol.iterator ]() {
-				return { next() {
+			inc = (cv) => ++cv;
+
+		let fruitIter = { 
+			[ Symbol.iterator ]: function*() {
+				for(;;) {
 					cur = cur.map(inc);
 					cur = cur.concat( new Array( cur.reduce(sum) ).fill(0) );
 					++ii;
-					return { done: false, value: cur };
-				} };
-		} };
-		//console.log("Week " + ii + " | " + cur.slice(0, 80).join(" "));
+					yield cur;
+				} 
+			}
+		}; 
+	
+		console.log("Week " + ii + " | " + cur.slice(0, 80).join(" "));
 		for ( let a of fruitIter) {
-			//console.log("Week " + ii + " | " + a.slice(0, 80).join(" "));
+			console.log("Week " + ii + " | " + a.slice(0, 80).join(" "));
 			if ( ( cur.reduce(sum) ) >= population ) break;
 		}
 		return ii;
 	}
-}
+};
 module.exports = magicFruit_module;
