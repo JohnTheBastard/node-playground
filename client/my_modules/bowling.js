7a35dda1e3ad66d3d6ee1ed5d1b2a0995f313d86
John Hearn <jhearn@gmail.com>
2016-01-28 13:32:03 -0800

    replaced double quotes with single quotes (bad habit)

--- client/my_modules/bowling.js
+++ client/my_modules/bowling.js
@@ -1,27 +1,27 @@
 /* calculate a bowling score */
 'use strict';
-let rubric = { "-": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, 
-			   "6": 6, "7": 7, "8": 8, "9": 9, "X": 10, "x": 10 };
+let rubric = { '-': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, 
+			   '6': 6, '7': 7, '8': 8, '9': 9, 'X': 10, 'x': 10 };
 
 let bowling_module = {
 	score(sheet) {
-		let frames = sheet.split(" ");
+		let frames = sheet.split(' ');
 		// join first 9 frames, split into throws, get count
-		let throwsInFirstNine = frames.slice(0,9).join("").split("").length; 
-		let everyThrow = frames.join("").split("");
-		let lastFrame = frames[9].split("");
+		let throwsInFirstNine = frames.slice(0,9).join('').split('').length; 
+		let everyThrow = frames.join('').split('');
+		let lastFrame = frames[9].split('');
 		let total = 0;
 		let lastFrameScore = 0;
 
 		// calculate the score for the first nine frames
 		for( let ii = 0; ii < throwsInFirstNine; ii++  ) {
-			if ( everyThrow[ii] === "/" ) {
+			if ( everyThrow[ii] === '/' ) {
 				total += (10 - rubric[ everyThrow[ii-1] ]) + rubric[ everyThrow[ii+1] ];
-			} else if( everyThrow[ii] === ("X"||"x") ) {
+			} else if( everyThrow[ii] === ('X'||'x') ) {
 				// add this throw and the next, no matter what
 				total += (10 + rubric[everyThrow[ii+1]]);
 				// deal with whether the second bonus throw is a spare
-				if( everyThrow[ii+2] === "/" ) total += (10 - rubric[ everyThrow[ii+1] ]);
+				if( everyThrow[ii+2] === '/' ) total += (10 - rubric[ everyThrow[ii+1] ]);
 				else total += rubric[everyThrow[ii+2]];
 			} else {
 				total += rubric[ everyThrow[ii] ];
@@ -29,10 +29,10 @@ let bowling_module = {
 		}
 		
 		// now calculate the score for last frame
-		if( lastFrame[0] === ("X"||"x") ) {
-			if( lastFrame[2] === "/" ) lastFrameScore = 20;
+		if( lastFrame[0] === ('X'||'x') ) {
+			if( lastFrame[2] === '/' ) lastFrameScore = 20;
 			else  lastFrameScore = 10 + rubric[ lastFrame[1] ] + rubric[lastFrame[2]];
-		} else if ( lastFrame[1] === "/" ) {
+		} else if ( lastFrame[1] === '/' ) {
 			lastFrameScore = 10 + rubric[ lastFrame[2] ];
 		} else {
 			lastFrameScore = rubric[ lastFrame[0] ] + rubric[ lastFrame[1] ];
--- client/test/bowlingTests.js
+++ client/test/bowlingTests.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 let assert = require( 'chai' ).assert;
 const bowling = require( '../my_modules/bowling' );
 
-let badGame = "33 33 33 33 33 33 33 33 33 33";         // 6+6+6+6+6+6+6+6+6+6 = 60
-let perfect = "X X X X X X X X X XXX";                 // 30x10 = 300
-let noStrikes = "23 -/ 9- 5- 8/ 9- 9- 81 1- 4/-";      // 5+19+9+5+19+9+9+9+1+10 = 95
-let spareOnLastThrow = "X -/ X 5- 8/ 9- X 81 1- X4/";  // 20+20+15+5+19+9+19+9+1+20 = 137
-let strikeOnLastThrow = "X -/ X 5- 8/ 9- X 81 1- 4/X"; // 20+20+15+5+19+9+19+9+1+20 = 137
-let penultimateSpare = "62 71 X 9- 8/ X X 35 72 5/8";  // 8+9+19+9+20+23+18+8+9+18 = 140
+let badGame = '33 33 33 33 33 33 33 33 33 33';         // 6+6+6+6+6+6+6+6+6+6 = 60
+let perfect = 'X X X X X X X X X XXX';                 // 30x10 = 300
+let noStrikes = '23 -/ 9- 5- 8/ 9- 9- 81 1- 4/-';      // 5+19+9+5+19+9+9+9+1+10 = 95
+let spareOnLastThrow = 'X -/ X 5- 8/ 9- X 81 1- X4/';  // 20+20+15+5+19+9+19+9+1+20 = 137
+let strikeOnLastThrow = 'X -/ X 5- 8/ 9- X 81 1- 4/X'; // 20+20+15+5+19+9+19+9+1+20 = 137
+let penultimateSpare = '62 71 X 9- 8/ X X 35 72 5/8';  // 8+9+19+9+20+23+18+8+9+18 = 140
 
 describe( 'bowling.score method should accurately calculate bowling scores ', function() {
 	it( 'should work on games with no spares or strikes', function(){
