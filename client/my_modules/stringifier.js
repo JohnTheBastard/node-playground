b5cebcab2eb356f1e24c73a239a97fe12f5f2420
John Hearn <jhearn@gmail.com>
2016-02-05 11:14:14 -0800

    stringifier working (utility for boxxle clone level data)

--- client/my_modules/stringifier.js
+++ client/my_modules/stringifier.js
@@ -1,5 +1,29 @@
 'use strict';
 
-let XXXXXXX = require( './xxxxxxx' ).XXXXXXX;
+var pad = function (num, size) {
+	var s = num+"";
+	while (s.length < size) s = "0" + s;
+	return s;
+};
 
-console.log(  XXXXXXX("foo") );
+
+let stringifier_module = {
+	stringifier: function(data) {
+		let stringifiedData = {};
+		
+		data.easy.forEach(function(level, index) {
+			let id = "easyPack001-level" + pad(index, 3);
+			stringifiedData[id] = JSON.stringify(level);
+		});
+		
+		data.hard.forEach(function(level, index) {
+			let id = "hardPack001-level" + pad(index, 3);
+			stringifiedData[id] = JSON.stringify(level);
+		});
+		
+		
+		return stringifiedData;
+	}
+};
+
+module.exports = stringifier_module;
